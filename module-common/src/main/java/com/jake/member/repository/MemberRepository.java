13f01bfb57dba97c7c368ea33b062d6ebc10f3c1
jake <gerion@gmail.com>
2024-02-01 14:31:39 +0900

    rename

--- module-common/src/main/java/com/jake/member/repository/MemberRepository.java
+++ module-common/src/main/java/com/jake/member/repository/MemberRepository.java
@@ -3,5 +3,5 @@ package com.jake.member.repository;
 import com.jake.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberService extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
